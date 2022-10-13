a4f31d213fb659b3235da41b9742371a57f79abf
AriyaUnni <ariyaunni2000@gmail.com>
2022-10-14 00:30:45 +0530

    last commit

--- src/app/viewfriend/viewfriend.component.ts
+++ src/app/viewfriend/viewfriend.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-viewfriend',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewfriendComponent implements OnInit {
 
-  constructor() { }
+  constructor(private myapi:ApiService) { 
 
-  friendsData=[]
+    this.fetchData()
+  }
+
+  fetchData=()=>{
+    this.myapi.ViewFriendData().subscribe(
+      (data)=>{
+        this.friendsData=data
+      }
+    )
+  }
+
+  friendsData:any=[]
 
   ngOnInit(): void {
   }
